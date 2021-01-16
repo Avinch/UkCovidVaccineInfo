2e170d739ad0b97fe70f81cdc4d5b65ecf2892cb
Sam Scott <sam.scott@blacklightsoftware.com>
2021-01-16 19:57:25 +0000

    init commit

---
+++ src/UkCovidVaccineInfo/UkCovidVaccineInfo/Data/VaccinationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UkCovidVaccineInfo.Data
+{
+    public class VaccinationInfo
+    {
+        public DateTime Date { get; set; }
+        public int? NewFirstDose { get; set; }
+        public int? CumFirstDose { get; set; }
+        public int? NewSecondDose { get; set; }
+        public int? CumSecondDose { get; set; }
+
+        public int? TotalNew => NewFirstDose + NewSecondDose;
+        public int? Total => CumFirstDose + CumSecondDose;
+    }
+}
